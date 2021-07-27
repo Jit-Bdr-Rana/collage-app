24278a21a4b5179101b13b78354138d7fafd7436
jit rana <jitbdrrana8@gmail.com>
2021-07-27 19:58:53 +0545

    home page setup

---
+++ src/main/java/com/college/controller/HomePageController.java
@@ -0,0 +1,15 @@
+package com.college.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomePageController {
+	
+	@RequestMapping("")
+	public String homePage(Model model) {
+		return "front/index";
+	}
+
+}
